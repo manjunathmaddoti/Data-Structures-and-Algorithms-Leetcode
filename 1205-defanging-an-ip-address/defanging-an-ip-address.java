40ddeff0e5757f108f515f050160076c053c5f88
𝕄𝔸𝔻𝔻𝕆𝕋𝕀 𝕄𝔸ℕ𝕁𝕌ℕ𝔸𝕋ℍ <manjunathmaddoti@gmail.com>
2026-01-03 10:01:30 +0530

    ip address

--- 1205-defanging-an-ip-address/defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -2,7 +2,7 @@ class Solution {
     public String defangIPaddr(String address) {
         
         String ans = "";
-        for(int i=0; i<address.length(); i++){
+        for(int i = 0; i<address.length(); i++){
             char ch = address.charAt(i);
             if(ch == '.'){
                 ans = ans + "[.]";
