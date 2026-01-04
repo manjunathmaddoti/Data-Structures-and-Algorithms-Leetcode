d23237af4a271f8ab7aadbe75a69c7866d2e541c
𝕄𝔸𝔻𝔻𝕆𝕋𝕀 𝕄𝔸ℕ𝕁𝕌ℕ𝔸𝕋ℍ <manjunathmaddoti@gmail.com>
2026-01-04 12:04:04 +0530

    Time: 2 ms (98.86%) | Memory: 44.6 MB (52.20%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // skip non-alphanumeric from left
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            // skip non-alphanumeric from right
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // compare characters (ignore case)
+            if (Character.toLowerCase(s.charAt(left)) 
+                != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;   
+        
+
+        
+    }
+}
